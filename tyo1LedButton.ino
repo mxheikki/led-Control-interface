0e7e5b6e3650076a4bf0fa6c274de058cb39afe9
mxheikki <MattiHeikkinen@Matti-MacBook.local>
2017-02-06 14:33:48 +0200

    test1
    testing versions

--- tyo1LedButton.ino
+++ tyo1LedButton.ino
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-
+// This is Comment
 // Your WiFi credentials.
 // Set password to "" for open networks.
 char ssid[] = "Your Wifi-Network Name";
@@ -18,7 +18,7 @@ void setup()
 }
 BLYNK_WRITE(V1) //Button Widget is writing to pin V1
 {
-  int pinData = param.asInt();  //values 0 or 1 
+  int pinData = param.asInt();  //values 0 or 1
   digitalWrite(D5, pinData);
 }
 void loop()
